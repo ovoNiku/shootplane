d03faa76801b017c0874e11f74482a0e428a4ef0
ovoNiku <2891247632@qq.com>
2019-11-07 17:24:11 +0800

    add new images obj

--- main.js
+++ main.js
@@ -25,19 +25,22 @@ var enableDebugMode = function (game, enable) {
 var __main = function () {
     var images = {
         plane: 'images/plane.png',
-        bullet: 'images/bullet.png',
+        bullet0: 'images/bullet0.png',
+        bullet1: 'images/bullet1.png',
         particle: 'images/particle.png',
-        bg0: 'images/bg0.png',
-        bg1: 'images/bg1.png',
+        bg: 'images/bg.png',
+        title: 'images/title.png',
+        title1: 'images/title1.png',
+        life: 'images/life.png',
         cloud0: 'images/cloud0.png',
         cloud1: 'images/cloud1.png',
         cloud2: 'images/cloud2.png',
         enemy0: 'images/enemy0.png',
         enemy1: 'images/enemy1.png',
         enemy2: 'images/enemy2.png',
-        enemy3: 'images/enemy3.png',
-        enemy4: 'images/enemy4.png',
-        enemy5: 'images/enemy5.png',
+        // enemy3: 'images/enemy3.png',
+        // enemy4: 'images/enemy4.png',
+        // enemy5: 'images/enemy5.png',
     }
     var game = new Game(30, images, function () {
         var s = new TitleScene(game)
